03a443b7960a48f7fc96630d1d82885b62fe704d
Aditya Pratama <orochimadit@gmail.com>
2019-02-08 15:12:39 +0700

    menambahkan create product

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,7 +4,16 @@
 
 @section('content') 
   <div class="row">
+  
     <div class="col-md-12">
+    <div class="row mb-3">
+  <div class="col-md-12 text-right">
+    <a
+      href="{{route('products.create')}}"
+      class="btn btn-primary"
+    >Create Product</a>
+  </div>
+</div>
       <table class="table table-bordered table-stripped">
         <thead>
           <tr>
