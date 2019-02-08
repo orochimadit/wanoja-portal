a75ea29631ac7a0bd7484cbce99bc7c209f9fe29
Aditya Pratama <orochimadit@gmail.com>
2019-02-09 02:50:58 +0700

    menambahkan manage order

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -74,7 +74,7 @@
               <li><a href="{{route('users.index')}}"><span class="oi oi-people"></span> Manage users</a></li>
               <li><a href="{{route('categories.index')}}"><span class="oi oi-tag"></span> Manage categories</a></li>
               <li><a href="{{route('products.index')}}"><span class="oi oi-book"></span> Manage product</a></li>
-
+              <li><a href="{{route('orders.index')}}"><span class="oi oi-inbox"></span> Manage orders</a></li>
               <div class="d-block d-md-none">
                   <div class="dropdown-divider"></div>
                   <li><a href="#"> Profile</a></li>
